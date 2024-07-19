ea3f6190badd398bc0cea5b546ad047e813a8409
okta013 <sindorayka@yandex.ru>
2024-07-19 11:52:25 +0300

    add suppliercontroller

---
+++ src/main/java/org/candyfactory/controller/SupplierController.java
@@ -0,0 +1,31 @@
+package org.candyfactory.controller;
+
+import org.candyfactory.dao.SupplierDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/suppliers")
+public class SupplierController {
+    private final SupplierDao supplierDao;
+
+    public SupplierController(SupplierDao supplierDao) {
+        this.supplierDao = supplierDao;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("suppliers", supplierDao.index());
+        return "suppliers/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("supplier", supplierDao.show(id));
+        return "suppliers/show";
+    }
+
+}
--- src/main/java/org/candyfactory/dao/SupplierDao.java
+++ src/main/java/org/candyfactory/dao/SupplierDao.java
@@ -1,7 +1,81 @@
 package org.candyfactory.dao;
 
+import org.candyfactory.model.Supplier;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class SupplierDao {
+public class SupplierDao  {
+    private static final String URL = "jdbc:postgresql://localhost:5432/candy_factory";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "ytpkbvtyz";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public List<Supplier> index() {
+        List<Supplier> suppliers = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Supplier";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Supplier supplier = new Supplier();
+
+                supplier.setId(resultSet.getInt("id"));
+                supplier.setName(resultSet.getString("name"));
+                supplier.setAddress(resultSet.getString("address"));
+
+                suppliers.add(supplier);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return suppliers;
+    }
+
+    public Supplier show(int id) {
+        Supplier supplier = null;
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            supplier = new Supplier();
+
+            supplier.setId(resultSet.getInt("id"));
+            supplier.setName(resultSet.getString("name"));
+            supplier.setAddress(resultSet.getString("address"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return supplier;
+    }
 }
